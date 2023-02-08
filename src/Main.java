a88bf0559b77a0a284604887d0a88a9bcce66290
ElliotRipa <elliot.ripa@gmail.com>
2023-02-08 21:59:04 +0100

    Added ability to read a table and return a list of rows.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://192.168.1.128:3306/media","desktop","&a90f#b33Sf0");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM movies");
+
+            int columnCount = rs.getMetaData().getColumnCount();
+            ArrayList<ArrayList<String>> list = new ArrayList<>();
+            int whileCounter = 0;
+
+            while(rs.next()) {
+
+                ArrayList<String> entry = new ArrayList<>();
+
+                for(int i = 1 ; i <= columnCount ; i++) {
+                    entry.add(rs.getString(i));
+                }
+
+                list.add(entry);
+
+            }
+            con.close();
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
     }
 
 }

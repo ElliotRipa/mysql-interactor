e4fef7e0f78d8715a5316974ec95481052a64da2
ElliotRipa <elliot.ripa@gmail.com>
2023-02-08 22:08:27 +0100

    Functionally decomposed Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,47 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<ArrayList<String>> result = readTable("movies");
+        System.out.println("Debug point.");
+    }
 
+    public static ArrayList<ArrayList<String>> readTable(String table) {
 
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://192.168.1.128:3306/media","desktop","&a90f#b33Sf0");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM movies");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM " + table);
 
             int columnCount = rs.getMetaData().getColumnCount();
             ArrayList<ArrayList<String>> list = new ArrayList<>();
-            int whileCounter = 0;
 
             while(rs.next()) {
 
-                ArrayList<String> entry = new ArrayList<>();
-
-                for(int i = 1 ; i <= columnCount ; i++) {
-                    entry.add(rs.getString(i));
-                }
-
-                list.add(entry);
+                list.add(readRow(rs, columnCount));
 
             }
             con.close();
 
+            return list;
 
         } catch (Exception e) {
             System.out.println(e);
         }
 
+        return null;
+
+    }
+
+    public static ArrayList<String> readRow(ResultSet rs, int columnCount) throws SQLException {
+
+        ArrayList<String> entry = new ArrayList<>();
+
+        for(int i = 1 ; i <= columnCount ; i++) {
+            entry.add(rs.getString(i));
+        }
+
+        return entry;
 
     }
 
